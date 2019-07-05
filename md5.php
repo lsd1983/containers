218c1dc52ccf4fd358108491712349e288e46447
lsd1983 <52343803+lsd1983@users.noreply.github.com>
2019-07-05 11:41:16 +0300

    Add files via upload

---
+++ md5.php
@@ -0,0 +1,41 @@
+
+<html>
+
+
+
+	<form action="" method="POST" >
+
+	<input type="text" name="text"/>
+	<input type="submit" name="submit"/ >
+
+
+	</form>	
+
+
+</html>
+
+<?php
+
+
+
+if (isset($_POST['text'])){
+
+	$md=$_POST['text'];
+	
+	if ($md!=''){
+	
+		echo md5($md);
+		echo 'done';
+	}
+
+
+
+} else {
+
+echo 'No String provided';
+
+}
+
+
+
+?>
